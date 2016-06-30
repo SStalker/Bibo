c04092818eeae1764db581193ed9902ec43c7d34
SStalker <raphael@bluepanel.org>
2016-07-01 01:23:39 +0200

    Added sql that fill automaticallicy data into the derby db

---
+++ src/main/resources/META-INF/sql/create.sql
@@ -0,0 +1,26 @@
+
+INSERT INTO LOGIN VALUES(1, 'email@email.de', 0, 'password', 'username')
+INSERT INTO ADDRESS VALUES(1, 45455, 'Straßße', '2a', 'Stadt ui')
+INSERT INTO PROFILE VALUES(1, 'Raphael', 'Grewe', 1)
+INSERT INTO T_MEMBER VALUES(1, 1, 1)
+
+INSERT INTO AUTHOR VALUES(1, 'DW', 'Weil', 'Dirk')
+INSERT INTO AUTHOR VALUES(2, 'MK', 'Michael', 'Kurz')
+INSERT INTO AUTHOR VALUES(3, 'MM', 'Martin', 'Marinschek')
+INSERT INTO AUTHOR VALUES(4, 'MC', 'Michal', 'Cmil')
+INSERT INTO AUTHOR VALUES(5, 'MM', 'Michal', 'Matloka')
+INSERT INTO AUTHOR VALUES(6, 'FM', 'Francesco', 'Marchioni')
+
+INSERT INTO PUBLISHER VALUES(1, 'Packt Publishing')
+INSERT INTO PUBLISHER VALUES(2, 'dpunkt.verlag')
+INSERT INTO PUBLISHER VALUES(3, 'entwickler.press')
+
+INSERT INTO DESCRIPTION VALUES(1, '1 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', 2, 2)
+INSERT INTO DESCRIPTION VALUES(2, '2 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', 1, 3)
+INSERT INTO DESCRIPTION VALUES(3, '3 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.', 6, 1)
+
+INSERT INTO BOOK VALUES('978-3-86490-009-9', 3, 'JavaServer Faces 2.2', 1)
+INSERT INTO BOOK VALUES('978-3-86802-103-5', 2, 'Java EE 7', 2)
+INSERT INTO BOOK VALUES('978-1-78217-198-0', 3, 'Java EE 7 Development with Wildfly', 3)
+
+
